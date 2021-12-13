ab42cac6c61ca88f24e1306965d6c2fc49342c42
Nick <nickvanoutersterp@kpnmail.nl>
2021-12-13 16:28:16 +0100

    cleaning up and implementing logger - RD-34

--- PG_API/Controllers/UserController.cs
+++ PG_API/Controllers/UserController.cs
@@ -11,7 +11,11 @@ namespace PG_API.Controllers;
 [ApiController]
 public class UserController : ControllerBase
 {
+    /// <summary>
+    /// create logging instance for UserController
+    /// </summary>
     private static readonly ILog _log = LogManager.GetLogger(typeof(UserController));
+    
     private readonly AsyncHandler _asyncHandler = new();
     private readonly HttpClient _httpClient = new();
     
@@ -19,17 +23,14 @@ public class UserController : ControllerBase
     [HttpPost()]
     public string Post()
     {
-        _log.Fatal("/user post");
-        _log.Error("/user post");
-        _log.Warn("/user post");
-        _log.Info("/user post");
-        _log.Debug("/user post");
-        _log.Trace("/user post", null);
+        _log.Trace("post request to /user", null);
         HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post,
             "https://portalgenius.maps.arcgis.com/sharing/rest/portals/x/users?f=json&token=K2n1Br9O90sp6IZm31EjWm4dkcn2AevsFhYyb4VqZ62P9pNIi8I4LuWVz6G48Yr3XIegSenUMRAro7TROBmjoFlyTghB4dz5BOC8OH7DuNTUZq76uTW3A2C98knFMFc_FCagtBFFwfR2Lcbrword6scyX1PbiFtWgMvSsILwHAEterwSs_Sv8NVR0ohfoIk1VCrwcYxpvylqsyi19HaAuA..&searchUserAccess=*&filter=*&num=100");
         HttpResponseMessage responseMessage = _httpClient.Send(requestMessage);
         string s = _asyncHandler.Read(responseMessage.Content).Result;
        //todo: implement JsonHandler();
+       
+       _log.Trace("returning post request to /user", null);
         return s;
     }
 }
