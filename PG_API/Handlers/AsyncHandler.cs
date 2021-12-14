7a312500f3fce5494899ab8f12f726340abd1ee5
Nick <nickvanoutersterp@kpnmail.nl>
2021-12-14 15:28:49 +0100

    RD-42 beginnetje, maar nog lang niet af

--- PG_API/Handlers/AsyncHandler.cs
+++ PG_API/Handlers/AsyncHandler.cs
@@ -6,4 +6,13 @@ public class AsyncHandler
     {
         return await httpContent.ReadAsStringAsync();
     }
+    public bool SetToken(HttpContent httpContent)
+    {
+        Task<string> task = Read(httpContent);
+        Console.WriteLine(task.Result);
+        //string token = task.Result.Substring(10, 216);
+        
+        //todo: store token
+        return true;
+    }
 }
