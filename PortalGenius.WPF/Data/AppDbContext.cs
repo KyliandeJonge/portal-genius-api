7736273916281b8f969bfdab4faf11949e3f4503
matthijs110 <matthijs110@users.noreply.github.com>
2021-12-15 10:52:19 +0100

    Samenvoeging + gitginore

---
+++ PortalGenius.WPF/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PortalGenius.WPF.Models;
+
+namespace PortalGenius.WPF.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Item> Items => Set<Item>();
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ PortalGenius.WPF/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System.ComponentModel;
+using System.Windows;
+
+namespace PortalGenius.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly IHost _host;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _host = Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(webHost =>
+                {
+                    webHost.UseStartup<ApiStartup>();
+                    webHost.UseKestrel();
+
+                    // Webhost URL's is configured via appsettings.{env}.json
+                })
+                .Build();
+
+            _host.Start();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            // Stop the Kestrel host when this window closes
+            _host.Dispose();
+
+            base.OnClosing(e);
+        }
+    }
+}
