d094dfaa28287f60720146cc325cb82ba00374ba
Kylian <x@x.x>
2021-12-20 09:04:39 +0100

    unneeded using

--- PortalGenius.WPF/ApiStartup.cs
+++ PortalGenius.WPF/ApiStartup.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using PortalGenius.WPF.Data;
 using System;
-using System.Diagnostics;
 using System.IO;
 
 namespace PortalGenius.WPF
