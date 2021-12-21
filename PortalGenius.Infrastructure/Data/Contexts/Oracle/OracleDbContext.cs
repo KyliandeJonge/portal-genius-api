6d3f3420d68e8aaf7d1e6d139c0fcc685dc47981
Matthijs <matthijs110@users.noreply.github.com>
2021-12-21 17:06:13 +0100

    Ondersteuning voor Oracle

---
+++ PortalGenius.Infrastructure/Data/Contexts/Oracle/OracleDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace PortalGenius.Infrastructure.Data
+{
+    public class OracleDbContext : AppDbContext
+    {
+        public OracleDbContext(IConfiguration configuration) : base(configuration)
+        {
+        }
+
+        public OracleDbContext(DbContextOptions<OracleDbContext> options, IConfiguration configuration) : base(options, configuration)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseNpgsql(Configuration.GetConnectionString("Oracle"));
+        }
+    }
+}
---
+++ PortalGenius.Infrastructure/Data/Contexts/Oracle/OracleDbContextFactory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace PortalGenius.Infrastructure.Data
+{
+    public class OracleDbContextFactory : AppDbContextFactory<OracleDbContext>
+    {
+        public override DbContextOptionsBuilder<OracleDbContext> UseDatabaseService(DbContextOptionsBuilder<OracleDbContext> builder, IConfigurationRoot configuration)
+        {
+            return builder.UseOracle(configuration.GetConnectionString("Oracle"));
+        }
+    }
+}
