e7980cc09d3495ae4b6e0f1beb0920f6f806050d
Matthijsn <44608102+Matthijsn@users.noreply.github.com>
2021-12-22 14:12:10 +0100

    Rd 70: Items ophalen uit de database (#8)
    * ik probeer een pagina toe te voegen om data te laten zien. nog niet zeker of het een page of een window moet zijn xx
    
    * Knop toegevoegd
    
    * Dependency Injection binnen Window (#4)
    
    * Begin met DI (werkt nog niet)
    
    * ShowAPI met DI
    
    * Update MainWindow.xaml.cs
    
    * treeview toegevoegd
    
    Co-authored-by: lisannehsmn <44570588+lisannehsmn@users.noreply.github.com>
    Co-authored-by: Matthijs <matthijs110@users.noreply.github.com>

---
+++ PortalGenius.WPF/ShowAPIoutput.xaml.cs
@@ -0,0 +1,29 @@
+﻿using PortalGenius.Core.Services;
+using JSONTreeView;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PortalGenius.WPF
+{
+    /// <summary>
+    /// Interaction logic for ShowAPIoutput.xaml
+    /// </summary>
+    public partial class ShowAPIoutput : Page
+    {
+        private readonly ArcGISService _arcGISService;
+
+        public ShowAPIoutput(ArcGISService arcGISService)
+        {
+            _arcGISService = arcGISService;
+
+            InitializeComponent();
+        }
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //UserController U1 = new UserController();
+            //ItemController itemcontroller = new ItemController();
+            //Output.Text = itemcontroller.Post();
+            Data.ProcessJson((await _arcGISService.GetAllItems()).ToString());
+        }
+    }
+}
