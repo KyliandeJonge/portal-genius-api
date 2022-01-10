cd5ce49a2cc8d7dfc856186b1e7904b0d2919311
Matthijs <matthijs110@users.noreply.github.com>
2022-01-10 15:39:44 +0100

    Post test

--- tests/PortalGenius.UnitTests/Core/Services/HttpServiceTests/HttpServiceTests_PostAsync.cs
+++ tests/PortalGenius.UnitTests/Core/Services/HttpServiceTests/HttpServiceTests_PostAsync.cs
@@ -1,5 +1,8 @@
-﻿using Moq.Contrib.HttpClient;
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using Moq.Contrib.HttpClient;
 using Newtonsoft.Json;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
@@ -13,7 +16,7 @@ namespace PortalGenius.UnitTests.Core.Services
         }
 
         [Fact]
-        public async Task PostAsync_ShouldYYYYYY_WhenZZZZZ()
+        public async Task PostAsync_ShouldReturnData_WhenResponseIsSuccess()
         {
             // Arrange
             var responseBody = new { Test = "Name" };
@@ -30,5 +33,116 @@ namespace PortalGenius.UnitTests.Core.Services
             Assert.NotNull(result);
         }
 
+        [Fact]
+        public async Task PostAsync_ShouldLogWarning_WhenHttpResponseIsError()
+        {
+            // Arrange
+            var endpoint = $"{ApiBaseUrl}/test-endpoint";
+            var statusCode = HttpStatusCode.BadRequest;
+
+            var responseBody = new { Test = "Name" };
+            var content = JsonConvert.SerializeObject(responseBody);
+
+            _httpHandlerMock.SetupRequest(HttpMethod.Post, endpoint)
+                .ReturnsResponse(statusCode);
+
+            // Act
+            var result = await _httpService.PostAsync<object>("test-endpoint", new StringContent(content));
+
+            // Assert
+            _httpServiceLogger.VerifyLog(logger => logger.LogWarning("[HTTP POST {statusCode}] Something went wrong while connecting with: ({apiUrl}).", statusCode, endpoint));
+        }
+
+        [Fact]
+        public async Task PostAsync_ShouldReturnNull_WhenHttpResponseIsError()
+        {
+            // Arrange
+            var endpoint = $"{ApiBaseUrl}/test-endpoint";
+            var statusCode = HttpStatusCode.BadRequest;
+
+            var responseBody = new { Test = "Name" };
+            var content = JsonConvert.SerializeObject(responseBody);
+
+            _httpHandlerMock.SetupRequest(HttpMethod.Post, endpoint)
+                .ReturnsResponse(statusCode);
+
+            // Act
+            var result = await _httpService.PostAsync<object>("test-endpoint", new StringContent(content));
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task PostAsync_ShouldReturnNull_WhenHttpRequestExceptionThrown()
+        {
+            // Arrange
+            var responseBody = new { Test = "Name" };
+            var content = JsonConvert.SerializeObject(responseBody);
+
+            _httpHandlerMock.SetupRequest(HttpMethod.Post, $"{ApiBaseUrl}/test-endpoint")
+                .Throws<HttpRequestException>();
+
+            // Act
+            var result = await _httpService.PostAsync<object>("test-endpoint", new StringContent(content));
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task PostAsync_ShouldLogError_WhenHttpRequestExceptionThrown()
+        {
+            // Arrange
+            var endpoint = $"{ApiBaseUrl}/test-endpoint";
+
+            var responseBody = new { Test = "Name" };
+            var content = JsonConvert.SerializeObject(responseBody);
+
+            _httpHandlerMock.SetupRequest(HttpMethod.Post, endpoint)
+                .Throws<HttpRequestException>();
+
+            // Act
+            var result = await _httpService.PostAsync<object>("test-endpoint", new StringContent(content));
+
+            // Assert
+            _httpServiceLogger.VerifyLog(logger => logger.LogError("[HTTP POST 500] Error while connecting with: ({apiUrl}).", endpoint));
+        }
+
+        [Fact]
+        public async Task PostAsync_ShouldReturnNull_WhenJsonReaderExceptionThrown()
+        {
+            // Arrange
+            var responseBody = new { Test = "Name" };
+            var content = JsonConvert.SerializeObject(responseBody);
+
+            _httpHandlerMock.SetupRequest(HttpMethod.Post, $"{ApiBaseUrl}/test-endpoint")
+                .Throws<JsonReaderException>();
+
+            // Act
+            var result = await _httpService.PostAsync<object>("test-endpoint", new StringContent(content));
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task PostAsync_ShouldLogError_WhenJsonReaderExceptionThrown()
+        {
+            // Arrange
+            var endpoint = $"{ApiBaseUrl}/test-endpoint";
+
+            var responseBody = new { Test = "Name" };
+            var content = JsonConvert.SerializeObject(responseBody);
+
+            _httpHandlerMock.SetupRequest(HttpMethod.Post, endpoint)
+                .Throws<JsonReaderException>();
+
+            // Act
+            var result = await _httpService.PostAsync<object>("test-endpoint", new StringContent(content));
+
+            // Assert
+            _httpServiceLogger.VerifyLog(logger => logger.LogError("Error while parsing JSON"));
+        }
     }
 }
