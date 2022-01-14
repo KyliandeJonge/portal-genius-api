9f605ec80a783c068824452c40c6323490287c3d
Matthijsn <44608102+Matthijsn@users.noreply.github.com>
2022-01-14 13:43:15 +0100

    opslaan functie toegevoegd

--- src/PortalGenius.WPF/MainWindow.xaml.cs
+++ src/PortalGenius.WPF/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿
+using Dasync.Collections;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Win32;
+using PortalGenius.Core.Data;
 using PortalGenius.Core.Interfaces;
 using PortalGenius.Core.Models;
 using PortalGenius.Core.Services;
@@ -11,6 +13,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace PortalGenius.WPF
@@ -99,13 +103,35 @@ namespace PortalGenius.WPF
             dgMainDg.ItemsSource = await _itemRepository.GetAllAsync();
         }
 
-        private void Opslaan_Click(object sender, RoutedEventArgs e)
+        private async void Opslaan_Click(object sender, RoutedEventArgs e)
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
 
+
             if (saveFileDialog.ShowDialog() == true)
             {
-               //string ID = dgMainDg.SelectedItems[0];
+                //string ID = ((Item)dgMainDg.SelectedItems[0]).Id;
+
+                var items = await _arcGISService.GetAllItemsAsync();
+
+                var dirname = Path.GetDirectoryName(saveFileDialog.FileName);
+
+                await items.ParallelForEachAsync(item =>
+                {
+                    string URI = $"https://portalgenius.maps.arcgis.com/sharing/rest/content/items/{item.Id}/data?f=json&token={UserData.genToken}";
+                    var path = $"{dirname}\\{item.Name}";
+                    using (WebClient wc = new())
+                    {
+                        wc.DownloadFileAsync(
+                                // Param1 = Link of file
+                                new System.Uri(URI),
+                                // Param2 = Path to save
+                                $"C:\\Users\\Matthijs\\Downloads\\Items\\{item.Name}"
+                            );
+                    }
+
+                    return Task.CompletedTask;
+                });
             }
         }
     }
